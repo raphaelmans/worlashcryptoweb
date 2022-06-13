e4e4cc6a639060113ff7558d391d7ff4b3bf1991
Raphael Mansueto <raphaelm@Raphaels-MacBook-Pro.local>
2022-06-14 00:00:11 +0800

    init codebase

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    ignorePatterns: ['!.*', 'dist', 'node_modules'],
+};
